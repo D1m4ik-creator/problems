f1274908cc23c86716e37dbd0a64540014361dbf
Dim4ik-creator <litvinov06062006@gmail.com>
2025-12-31 14:41:01 +0300

    add serializers

--- app/models.py
+++ app/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser.models):
-    pass
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, blank=True, null= True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    timezone = models.CharField(max_length=50, default="UTC")
+    telegram_id = models.CharField(max_length=32, blank=True, null=True, unique=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
--- problems/settings.py
+++ problems/settings.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$n&mtawz=5qxkw$s(0fvj!^t&a6_piyzhk0*_p9liflc7-i1n('
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -72,6 +65,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -87,9 +82,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
@@ -99,6 +91,14 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
